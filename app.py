d26b9888f6625b1747b1dae6ee27b7662b1ceb80
Anuraj Lad <anurajlad1399@gmail.com>
2026-09-18 22:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #print("AI_Report_Generator")
 from fastapi import FastAPI, UploadFile, File
-
+from ingestion import load_file
+from report_generator import generate_report
+import os
 
 app = FastAPI(
     title="_BUILDING_REPORT_GENERATOR_",    
@@ -19,7 +21,23 @@ def home():
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):           # he function user kadun input ghenyasathi aahe. to upload the file..
+                                                          # Make sure data folder exists
+    os.makedirs("data", exist_ok=True)
+
+                                                             # Create file path
+    file_path = f"data/{file.filename}"
+
+                                                             # Save uploaded file
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+                                                            # Read the file using ingestion.py
+    df = load_file(file_path)
+
+                                                           # Generate report using report_generator.py
+    report = generate_report(df)  
     return {
         "filename": file.filename,
         "message": "File uploaded succesfully..."
+         "report": report
     }
