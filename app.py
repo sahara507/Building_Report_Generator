bf107f30264b3156b1364c805b9678b111a7d982
Akshay Parade <akshayparade96@gmail.com>
2026-09-18 18:11:07 +0530

    Updated report generator

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("AI_Report_Generator")
+#print("AI_Report_Generator")
+from fastapi import FastAPI, UploadFile, File
+
+
+app = FastAPI(
+    title="_BUILDING_REPORT_GENERATOR_",    
+    description="Generates the reports using Gen_AI",
+    
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Automated Report Generator is running"
+    }
+    
+
+
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):           # he function user kadun input ghenyasathi aahe. to upload the file..
+    return {
+        "filename": file.filename,
+        "message": "File uploaded succesfully..."
+    }
