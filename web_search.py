9ad1143e4b6dff60255bb0476beba33d7e65f2a1
Anuraj Lad <anurajlad1399@gmail.com>
2026-09-20 19:06:05 +0530

    Implement web search using TavilySearch
    Added functionality to perform web searches using Tavily.

--- web_search.py
+++ web_search.py
@@ -1,1 +1,24 @@
+#pip install tavily-python langchain-tavily        for active virtual environment
 
+#Tavily account madhe API key tayar karun ti .env file madhe thevli pahije
+
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+# Web search karanyasathi Tavily tool varato.
+web_search = TavilySearch(
+    max_results=5
+)
+
+
+# User search query web varati search karato.
+def search_web(query: str):
+
+    result = web_search.invoke({
+        "query": query
+    })
+
+    return result
